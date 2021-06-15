5af675b454203cbbbf5234ec811c94f57bd24bfc
Vaji77 <85842145+Vaji77@users.noreply.github.com>
2021-06-15 17:46:05 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,27 @@
+# 1.create three variables with the same value
+
+a = b = c = 30
+
+# a.divide a by 10
+divide = a/10
+print(int(divide))
+
+# b.multiply b by 50
+multiply = b * 50
+print(multiply)
+
+# c.add c value by 60
+add = c + 60
+print(add)
+
+# 2.create a string variable of 5 characters and replace 3rd character with G
+variable = "dhoni"
+x = variable.replace(variable[2],"G")
+print(x)
+
+# 3.create two values of int float datatypes and covert them viceversa
+a,b = 7,10.0
+print(a,",",b)
+a = (float(a))
+b = (int(b))
+print(a,",",b)
