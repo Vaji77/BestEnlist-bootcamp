3fb88cabc65a9b4821bae7b7e152e07f6e4284a4
Vaji77 <85842145+Vaji77@users.noreply.github.com>
2021-07-11 22:24:48 +0530

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,9 @@
+# Enumerate a python list and print the counter with the list value
+list_1= ['Apple','Ball','Cat','Dog','Egg']
+print("List:",list_1)
+print("Counter with list value:",list(enumerate(list_1)))
+
+# Enumerate a python tuple print the counter with the tuple value
+tuple_1=('Frog','Giraffe','Hat','Ink','Jaguar')
+print("Tuple:",tuple_1)
+print("Counter with tuple value",tuple(enumerate(tuple_1)))
